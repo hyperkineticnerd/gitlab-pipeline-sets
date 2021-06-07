9b8421e1c39f12c7e1694d47a8ce6b17ded37e0c
Sean Nelson <hyperkineticnerd@gmail.com>
2021-06-07 17:06:05 -0600

    update subtree.sh

--- subtree.sh
+++ subtree.sh
@@ -12,10 +12,10 @@
 # git fetch subtree_containers-podman
 # git fetch subtree_containers-container-build
 
-git subtree add --prefix=gitlab-pipelines/pipelines subtree_pipelines master --squash
-git subtree add --prefix=gitlab-pipelines/helpers subtree_helpers master --squash
-git subtree add --prefix=gitlab-pipelines/containers/podman subtree_containers-podman master --squash
-git subtree add --prefix=gitlab-pipelines/containers/container-build subtree_containers-container-build master --squash
+# git subtree add --prefix=gitlab-pipelines/pipelines subtree_pipelines master --squash
+# git subtree add --prefix=gitlab-pipelines/helpers subtree_helpers master --squash
+# git subtree add --prefix=gitlab-pipelines/containers/podman subtree_containers-podman master --squash
+# git subtree add --prefix=gitlab-pipelines/containers/container-build subtree_containers-container-build master --squash
 
 # git subtree push --prefix gitlab-pipelines/pipelines pipelines master
 # git subtree push --prefix gitlab-pipelines/helpers helpers master
