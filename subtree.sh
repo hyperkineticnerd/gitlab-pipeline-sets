7da5ee36c1850b429d2fe86c851cc45c1ac2b3ec
Sean Nelson <hyperkineticnerd@gmail.com>
2021-06-08 14:25:04 -0600

    add ansible as subtree

--- subtree.sh
+++ subtree.sh
@@ -2,26 +2,33 @@
 
 function subtree_add() {
     git remote add github git@github.com:hyperkineticnerd/gitlab-pipeline-sets.git
-    git remote add subtree_pipelines git@gitlab.developer.hkn.lab:gitlab-pipelines/pipelines.git
-    git remote add subtree_helpers git@gitlab.developer.hkn.lab:gitlab-pipelines/helpers.git
-    git remote add subtree_containers-podman git@gitlab.developer.hkn.lab:gitlab-pipelines/containers/podman.git
-    git remote add subtree_containers-container-build git@gitlab.developer.hkn.lab:gitlab-pipelines/containers/container-build.git
-
     git fetch github
-    git fetch subtree_pipelines
-    git fetch subtree_helpers
-    git fetch subtree_containers-podman
-    git fetch subtree_containers-container-build
 
+    git remote add subtree_pipelines git@gitlab.developer.hkn.lab:gitlab-pipelines/pipelines.git
+    git fetch subtree_pipelines
     git subtree add --prefix=gitlab-pipelines/pipelines subtree_pipelines master
+
+    git remote add subtree_helpers git@gitlab.developer.hkn.lab:gitlab-pipelines/helpers.git
+    git fetch subtree_helpers
     git subtree add --prefix=gitlab-pipelines/helpers subtree_helpers master
+
+    git remote add subtree_containers-ansible git@gitlab.developer.hkn.lab:gitlab-pipelines/containers/ansible.git
+    git fetch subtree_containers-ansible
+    git subtree add --prefix=gitlab-pipelines/containers/ansible subtree_containers-ansible master
+
+    git remote add subtree_containers-podman git@gitlab.developer.hkn.lab:gitlab-pipelines/containers/podman.git
+    git fetch subtree_containers-podman
     git subtree add --prefix=gitlab-pipelines/containers/podman subtree_containers-podman master
+
+    git remote add subtree_containers-container-build git@gitlab.developer.hkn.lab:gitlab-pipelines/containers/container-build.git
+    git fetch subtree_containers-container-build
     git subtree add --prefix=gitlab-pipelines/containers/container-build subtree_containers-container-build master
 }
 
 function subtree_pull() {
     git subtree pull --squash --prefix=gitlab-pipelines/pipelines subtree_pipelines master
     git subtree pull --squash --prefix=gitlab-pipelines/helpers subtree_helpers master
+    git subtree pull --squash --prefix=gitlab-pipelines/containers/ansible subtree_containers-ansible master
     git subtree pull --squash --prefix=gitlab-pipelines/containers/podman subtree_containers-podman master
     git subtree pull --squash --prefix=gitlab-pipelines/containers/container-build subtree_containers-container-build master
 }
@@ -29,6 +36,7 @@ function subtree_pull() {
 function subtree_push() {
     git subtree push --prefix gitlab-pipelines/pipelines subtree_pipelines master
     git subtree push --prefix gitlab-pipelines/helpers subtree_helpers master
+    git subtree push --prefix gitlab-pipelines/containers/ansible subtree_containers-ansible master --rejoin
     git subtree push --prefix gitlab-pipelines/containers/podman subtree_containers-podman master
     git subtree push --prefix=gitlab-pipelines/containers/container-build subtree_containers-container-build master
 }
